e21d0abe7df36bf809581a60ad93a537381189e8
p_zxxuzhang <zhangxinxu@zhangxinxu.com>
2018-08-30 14:10:02 +0800

    remove qcss
    remove qcss

--- run.js
+++ run.js
@@ -6,7 +6,7 @@
 	魔卡——高保真原型交付Node.js工具，主要功能：
 	1. HTML import功能，头部和尾部可以公用啦
 	2. 基于文件夹的CSS和JS资源合并策略
-	3. 支持qcss快速书写，变量以及@import模块引入
+	3. [本项已移除]支持qcss快速书写，变量以及@import模块引入
 	4. 本地http环境一键开启，post/get请求轻松模拟
  * @url https://github.com/zhangxinxu/mockup
  * @license MIT 保留原作者和原出处
@@ -229,279 +229,6 @@ const compile = function (src, dist) {
 };
 
 
-/*
-** qCss CSS快速书写
-** @params src qcss原始文件所在目录
-** @params dist 生成CSS文件所在目录
-*/
-
-let qCss = function (src, dist) {
-	// key转换
-	var keyMap = {
-		dn: 'display: none',
-		di: 'display: inline',
-		dib: 'display: inline-block',
-		db: 'display: block',
-		dt: 'display: table',
-		dtc: 'display: table-cell',
-		m: 'margin: ',
-		ml: 'margin-left: ',
-		mt: 'margin-top: ',
-		mr: 'margin-right: ',
-		mb: 'margin-bottom: ',
-		ma: 'margin: auto',
-		mla: 'margin-left: auto',
-		mra: 'margin-right: auto',
-		p: 'padding: ',
-		pl: 'padding-left: ',
-		pt: 'padding-top: ',
-		pr: 'padding-right: ',
-		pb: 'padding-bottom: ',
-		l: 'float: left',
-		r: 'float: right',
-		bg: 'background: ',
-		bgc: 'background-color: ',
-		bgi: 'background-image: ',
-		bgr: 'background-repeat: ',
-		bgp: 'background-position: ',
-		c: 'color: ',
-		bd: 'border: ',
-		bdl: 'border-left: ',
-		bdr: 'border-right: ',
-		bdt: 'border-top: ',
-		bdb: 'border-bottom: ',
-		br: 'border-radius: ',
-		bbb: 'box-sizing: border-box',
-		o: 'outline: ',
-		f: 'font-size: ',
-		ff: 'font-family: ',
-		fs: 'font-style: ',
-		fw: 'font-weight: ',
-		b: 'font-weight: bold',
-		i: 'font-style: italic',
-		n: 'font-weight: normal; font-style: normal',
-		tdl: 'text-decoration: underline',
-		tdn: 'text-decoration: none',
-		tc: 'text-align: center',
-		tl: 'text-align: left',
-		tr: 'text-align: right',
-		tj: 'text-align: justify',
-		cl: 'clear: both',
-		abs: 'position: absolute',
-		rel: 'position: relative',
-		fix: 'position: fixed',
-		op: 'opacity: ',
-		z: 'zoom: ',
-		zx: 'z-index: ',
-		h: 'height: ',
-		w: 'width: ',
-		minw: 'min-width: ',
-		maxw: 'max-width: ',
-		minh: 'min-height: ',
-		maxh: 'max-height: ',
-		lh: 'line-height: ',
-		v: 'vertical-align: ',
-		vt: 'vertical-align: top',
-		vm: 'vertical-align: middle',
-		vb: 'vertical-align: bottom',
-		poi: 'cursor: pointer',
-		def: 'cursor: default',
-		ovh: 'overflow: hidden',
-		ova: 'overflow: auto',
-		vh: 'visibility: hidden',
-		vv: 'visibility: visible',
-		prew: 'white-space: pre-wrap',
-		pre: 'white-space: pre',
-		nowrap: 'white-space: nowrap',
-		bk: 'word-break: break-all',
-		bkw: 'word-wrap: break-word',
-		ws: 'word-spacing: ',
-		ls: 'letter-spacing: ',
-		a: 'animation: ',
-		tsf: 'transform: ',
-		tsl: 'transition: ',
-		bs: 'box-shadow: ',
-		ts: 'text-shadow: ',
-		con: 'content: ',
-		center: 'position: absolute; top: 0; bottom: 0; right: 0; left: 0; margin: auto',
-		ell: 'text-overflow: ellipsis; white-space: nowrap; overflow: hidden',
-		clip: 'position: absolute; clip: rect(0 0 0 0)'
-	};
-
-	var valueMap = {
-		s: 'solid',
-		d: 'dashed',
-		tt: 'transparent',
-		cc: 'currentColor',
-		n: 'normal',
-		c: 'center',
-		rx: 'repeat-x',
-		ry: 'repeat-y',
-		no: 'no-repeat',
-		ih: 'inherit',
-		cb: 'content-box',
-		bb: 'border-box',
-		pb: 'padding-box',
-		l: 'left',
-		t: 'top',
-		r: 'right',
-		b: 'bottom'
-	};
-
-	fs.readdirSync(src).forEach(function (filename) {
-		var pathQcssFile = path.join(src, filename);
-		var st = fs.statSync(pathQcssFile);
-		// 下划线开头的qcss文件不处理
-		if (/\.qcss$/.test(filename) && /^_/.test(filename) == false) {
-			// .qcss文件才处理
-			// 读文件内容
-			var data = fs.readFileSync(pathQcssFile, {
-				// 需要指定编码方式，否则返回原生buffer
-				encoding: 'utf8'
-			});
-
-			// @import处理
-			data = data.replace(/@import\s+([\w\W]*?);/g, function (matchs, url) {
-				if (/\.qcss/.test(url)) {
-					// 过滤引号，括号，或者url等
-					url = url.replace(/url|\'|\"|\(|\)/g, '');
-					// 判断qcss文件是否有
-					var pathImportQcss = path.join(src, url);
-
-					if (!fs.existsSync(pathImportQcss)) {
-						console.error(pathImportQcss + '文件不存在，忽略这段引入');
-						return '/* '+ matchs +' */';
-					}
-					// 替换成对应文件内容
-					return fs.readFileSync(pathImportQcss);
-				}
-				// 非.qcss格式原路返回
-				return matchs;
-			});
-
-			// 计算出文件中设置的映射
-			var valueMapCustom = {};
-
-			data.replace(/\/\*([\w\W]*?)\*\//g, function (matchs, $1) {
-				$1.split(';').forEach(function (parts) {
-					var needPart = parts.split('$')[1];
-			  		if (needPart && needPart.split(/=|:/).length == 2) {
-						var keyValue = needPart.split(/=|:/);
-						if (keyValue[1].trim() && keyValue[0].trim()) {
-				  			valueMapCustom[keyValue[0].trim()] = keyValue[1].trim();
-						}
-			  		}
-				});
-			});
-		  	// base64 protect
-		  	data = data.replace(/;base64,/g, '%%%%%%');
-
-		  	var dataReplace = data.replace(/\{([\w\W]*?)\}/g, function (matchs, $1) {
-					// 删除声明块中的/**/注释
-				$1 = $1.replace(/\/\*([\w\W]*?)\*\//g, '');
-
-				var space = '    ';
-				var prefix = '{\n' + space, suffix = '\n}';
-				// 查询语句处理
-				if (/\{/.test($1)) {
-					suffix = '\n' + space + '}';
-					space = space + space;
-				  	prefix = '{' + $1.split('{')[0] + '{\n' + space;
-
-				  	$1 = $1.split('{')[1];
-				}
-				// 替换
-				// 分号是分隔符
-				return prefix + $1.split(';').map(function (state) {
-					state = state.trim();
-					if (!state) {
-						return '';
-					}
-					if (state.indexOf(':') != -1) {
-						return state;
-					}
-					// state指一段声明，例如f 20，此时下面的key是f, value是20
-					return state.replace(/^([a-z]+)(.*)$/g, function (matchs, key, value) {
-						// 值主要是增加单位，和一些关键字转换
-						// 1. 逗号
-						value = (value || '').split(',').map(function (multiple) {
-							return (multiple || '').split(' ').map(function (parts) {
-								parts = parts.trim();
-								if (!parts) {
-									return '';
-								}
-
-								if (key == 'l') {
-									key = 'left: ';
-								} else if (key == 't') {
-									key = 'top: ';
-								} else if (key == 'r') {
-									key = 'right: ';
-								} else if (key == 'b') {
-									key = 'bottom: ';
-								}
-
-								if (!isNaN(parts)) {
-									// 数值自动加px单位
-									// 不包括行高
-									if (key == 'lh' && parts < 5) {
-										return parts;
-									} else if (/^(?:zx|opa|z|fw)$/.test(key) == false && parts != '0' && /^calc/.test(multiple.trim()) == false) {
-										parts = parts + 'px';
-									}
-								} else if (key == 'tsl') {
-									// transition过渡
-									parts = (keyMap[parts] || parts).replace(':', '').trim();
-								} else if (key != 'a') {
-									// CSS动画不对值进行替换
-									parts = valueMapCustom[parts] || valueMap[parts] || parts;
-								}
-								return parts;
-							}).join(' ');
-						}).join(', ');
-
-						// 键转换
-						if (/:/.test(key) == false) {
-							key = keyMap[key] || key + ': ';
-						}
-						value = value.trim();
-						// 对齐美化
-						if (!value) {
-							key = key.split(';').map(function (beauty) {
-								return beauty.trim().replace(/:\s+/, ': ');
-							}).join(';\n' + space);
-						}
-
-						return key + value;
-				  	});
-				}).join(';\n' + space).trim() + suffix;
-		  	}).replace(/\w\{/g, function (matchs) {
-			  	return matchs.replace('{', ' {');
-			}).replace(/\}(\.|#|\:|\[|\w)/g, function (matchs) {
-			  	return matchs.replace('}', '}\n');
-			}).replace(/^\s{8}/gm, '    ').replace(/^\s{4}\}/gm, '}');
-
-			// base64 back
-			dataReplace = dataReplace.replace(/%%%%%%/g, ';base64,');
-
-			// 于是生成新的CSS文件
-			var newFilename = filename.replace('.qcss', '.css');
-			fs.writeFileSync(path.join(dist, newFilename), dataReplace, {
-				encoding: 'utf8'
-			});
-
-			console.log(newFilename + '生成成功！');
-		} else if (st.isDirectory()) {
-			// 如果是文件夹
-			var qcssDir = path.join(src, filename);
-			var cssDir = qcssDir.replace('qcss', 'css');
-			createPath(cssDir);
-			qCss(qcssDir, cssDir);
-		}
-	});
-};
-
-const pathSrcQCSS = './src/static/qcss/';
 const pathSrcCSS = './src/static/css/';
 const pathDistCSS = './dist/static/css/';
 const pathSrcJS = './src/static/js/';
@@ -511,13 +238,6 @@ const pathDistHTML = './dist/views/html/';
 
 // 任务
 const task = {
-	qcss: {
-		init: function () {
-			if (fs.existsSync(pathSrcQCSS)) {
-				qCss(pathSrcQCSS, pathSrcCSS);
-			}
-		}
-	},
 	css: {
 		init: function () {
 			// 资源清理
@@ -595,39 +315,21 @@ for (var keyTask in task) {
 }
 
 // 开启watch任务
-// QCSS监控任务
-if (fs.existsSync(pathSrcQCSS)) {
-	let timerQCSS;
-	fs.watch(pathSrcQCSS, {
-		recursive: true
-	}, (eventType, filename) => {
-		// 定时器让多文件同时变更只会只会执行一次合并
-		clearTimeout(timerQCSS);
-		console.log(filename + '发生了' + eventType + '变化');
-		timerQCSS = setTimeout(() => {
-			// qcss -> css
-			task.qcss.init();
-			// css合并
-			console.log('css编译并合并...');
-			task.css.init();
-		}, 100);
-	});
-} else {
-	let timerCSS;
-	// 监控CSS
-	fs.watch(pathSrcCSS, {
-		recursive: true
-	}, (eventType, filename) => {
-		// 定时器让多文件同时变更只会只会执行一次合并
-		clearTimeout(timerCSS);
-		console.log(filename + '发生了' + eventType + '变化');
-		timerCSS = setTimeout(() => {
-			// css合并
-			console.log('css合并...');
-			task.css.init();
-		}, 100);
-	});
-}
+// CSS监控任务
+let timerCSS;
+// 监控CSS
+fs.watch(pathSrcCSS, {
+	recursive: true
+}, (eventType, filename) => {
+	// 定时器让多文件同时变更只会只会执行一次合并
+	clearTimeout(timerCSS);
+	console.log(filename + '发生了' + eventType + '变化');
+	timerCSS = setTimeout(() => {
+		// css合并
+		console.log('css合并...');
+		task.css.init();
+	}, 100);
+});
 
 // JS监控任务
 let timerJS;
@@ -726,7 +428,7 @@ let server = http.createServer(function (request, response) {
 //设置监听端口
 let port = new Date().getFullYear();
 server.listen(port, '127.0.0.1', function () {
-	console.log('服务已经启动，端口为：' + port);
+	console.log('服务已经启动，访问地址为：http://127.0.0.1:'+ port +'/views/html/index.html');
 });
 
 
